987596eb4256492e14fd107d0efcaf83b0e2543f
Brandon Allred <b32alls@gmail.com>
2018-01-25 19:41:02 -0700

    massive db day 1 afternoon

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const massive=require('massive');
+const products_controller=require('./db/prodctrl/products_controller')
+require('dotenv').config();
+
+
+
+const app=express();
+app.use(bodyParser.json());
+app.use(cors());
+massive(process.env.CONNECTION_STRING).then(dbInstance => 
+    app.set('db', dbInstance)
+);
+
+app.post('/api/product', products_controller.create);
+app.get('/api/product/:id', products_controller.getOne);
+app.get('/api/products', products_controller.getAll);
+app.put('/api/product/:id', products_controller.update);
+app.delete('/api/product/:id', products_controller.delete);
+
+
+const port = process.env.PORT || 3232;
+app.listen(port,()=>{console.log(`Server listening on port: ${port}.`);});    
